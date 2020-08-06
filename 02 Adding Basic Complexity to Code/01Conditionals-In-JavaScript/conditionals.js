926ddd176f395fbef1e1ecdb0da0306087d5fc13
Supriya Sinha <supriya.sinha2k@gmail.com>
2020-08-06 13:56:29 +0530

    Add files via upload

---
+++ 02 Adding Basic Complexity to Code/01Conditionals-In-JavaScript/conditionals.js
@@ -0,0 +1,10 @@
+var temperature;
+
+// TODO: Go to Google and get the data.
+
+temperature = 20;
+console.log(temperature);
+
+if(temperature == 20) {
+    console.log("It's too cold outside!");
+}
