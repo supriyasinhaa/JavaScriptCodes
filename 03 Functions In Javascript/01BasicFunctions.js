681cbc46907d863829fc256f6201c4a5762de189
Supriya Sinha <supriya.sinha2k@gmail.com>
2020-08-07 03:12:49 +0530

    Add files via upload

---
+++ 03 Functions In Javascript/01BasicFunctions.js
@@ -0,0 +1,25 @@
+function sayHello(name){
+    console.log("Hello there! Supriya");
+    console.log("Hello there! ", name);
+}
+
+// sayHello();
+// sayHello();
+
+// sayHello("Riya");
+// sayHello("Priya");
+
+function sayHello(name){
+    console.log("Hello there! Supriya");
+    console.log(`Hello there! ${name}. How are you?`);
+}
+
+function namastey() {
+    return "Hello in India";
+}
+
+namastey();
+
+var greetings = namastey();
+console.log(namastey());
+console.log(greetings);
